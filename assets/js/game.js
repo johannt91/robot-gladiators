046278ecd53917840f320350320bee016730ea58
Johann Taylor <j.taylor1343@gmail.com>
2020-12-14 14:37:06 -0500

    Created functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+/* variable to store robot's name */
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+
+/* function to declare the start of the fight */
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+// fight();
