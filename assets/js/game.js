266005563d063bc4e2af3f7dbd52bacf339e13f3
Johann Taylor <j.taylor1343@gmail.com>
2020-12-18 21:12:02 -0500

    Fixed fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,5 +68,5 @@ var fight = function(enemyName) {
     for(var i = 0; i < enemyNames.length; i++) {
         var pickedEnemyName = enemyNames[i];
         enemyHealth = 50;
-        fight(enemyNames[i]);
+        fight(pickedEnemyName[i]);
     }
