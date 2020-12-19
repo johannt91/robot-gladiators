6b2f7e9fd768e5cf8457404258b2685b5e4f35e6
Johann Taylor <j.taylor1343@gmail.com>
2020-12-18 21:09:13 -0500

    Fixed fight loop to include other combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,5 +66,7 @@ var fight = function(enemyName) {
     };
 }
     for(var i = 0; i < enemyNames.length; i++) {
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
         fight(enemyNames[i]);
     }
