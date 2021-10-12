2216ea43cf183e99789247c85fa6d3b0dc64f87d
Johann Taylor <j.taylor1343@gmail.com>
2021-10-12 16:07:42 -0400

    Updated store logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -177,21 +177,18 @@ var fight = function (enemy) {
 var shop = function () {
     // ask player what they'd like to do
     var shopOptionPromt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for 'REFILL', 2 for 'UPGRADE', or 3 to 'LEAVE' to make a choice."
     );
+    shopOptionPromt = parseInt(shopOptionPromt);
 
     // use switch statements to carry out option
     switch (shopOptionPromt) {
-        case "REFILL": // new case
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-
-        case "UPGRADE": // new case
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
-        case "LEAVE": // new case
-        case "leave":
+        case 3:
             window.alert("Leaving the store");
             break;
         default:
